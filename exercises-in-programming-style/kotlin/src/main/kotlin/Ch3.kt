ac7283caea8eab22be695a00ce25d3b980f2da91
karl.styner <karl.styner@daumkakao.com>
2018-06-13 14:29:42 +0900

    eips ch.3 monolith

---
+++ exercises-in-programming-style/kotlin/src/main/kotlin/Ch3.kt
@@ -0,0 +1,63 @@
+class Pair(var first: String, var second: Int)
+
+fun main(args: Array<String>) {
+    val wordFreqs = mutableListOf<Pair>()
+
+    val stopWords = open("stop_words.txt").useLines { lines ->
+        lines.map { it.split(",") }.flatten().toList().map { it.toLowerCase() }
+    }
+
+    fun <E> MutableList<E>.swap(i1: Int, i2: Int) {
+        val tmp = this[i1]
+        this[i1] = this[i2]
+        this[i2] = tmp
+    }
+
+    open(inputFilename(args)).useLines { lines ->
+        lines.forEach { line ->
+            var startChar: Int? = null
+            var i = 0
+            for (c in line) {
+                if (startChar == null) {
+                    if (c.isLetterOrDigit()) {
+                        startChar = i
+                    }
+                } else {
+                    if (!c.isLetterOrDigit()) {
+                        var found = false
+                        val word = line.substring(startChar, i).toLowerCase()
+                        if (word !in stopWords) {
+                            var pairIndex = 0
+
+                            for (pair in wordFreqs) {
+                                if (word == pair.first) {
+                                    pair.second += 1
+                                    found = true
+                                    break
+                                }
+                                pairIndex += 1
+                            }
+
+                            if (!found) {
+                                wordFreqs.add(Pair(word, 1))
+                            } else if (wordFreqs.size > 1) {
+                                for (n in pairIndex downTo 0) {
+                                    if (wordFreqs[pairIndex].second > wordFreqs[n].second) {
+                                        wordFreqs.swap(n, pairIndex)
+                                        pairIndex = n
+                                    }
+                                }
+                            }
+                        }
+                        startChar = null
+                    }
+                }
+                i += 1
+            }
+        }
+    }
+
+    wordFreqs.take(25).forEachIndexed { i, pair ->
+        println("#${i+1}: ${pair.first} - ${pair.second}")
+    }
+}
--- exercises-in-programming-style/kotlin/src/main/kotlin/Util.kt
+++ exercises-in-programming-style/kotlin/src/main/kotlin/Util.kt
@@ -2,10 +2,16 @@ import java.io.File
 
 fun open(filename: String): File = File(object {}.javaClass.getResource(filename).toURI())
 
-fun inputFilename(array: Array<String>, default: String = "input.txt"): String {
+fun inputFilename(array: Array<String>, default: Input = Input.SHORT): String {
     return if (array.size > 1) {
         array[0]
     } else {
-        default
+        default.filename
     }
+}
+
+enum class Input(val filename: String) {
+    SHORT("input.txt"),
+    MEDIUM("pride-and-prejudice-head.txt"),
+    LONG("pride-and-prejudice.txt");
 }
